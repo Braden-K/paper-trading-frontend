564137314ef8294911ca78e0f52bfb92e0692fce
Braden Krajcirovic <bkraj@icloud.com>
2024-07-10 15:21:28 -0400

    perm search bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,23 +3,12 @@ import { SearchBar } from "./SearchBar";
 import { useState } from "react";
 
 export const Nav = () => {
-  const [showSearch, setShowSearch] = useState<boolean>(false);
-
   return (
     <div className="w-full h-16 bg-gray-900">
       <nav className="h-full flex items-center justify-between px-4">
         <div className="text-white">Paper Trading</div>
-        {showSearch && (
-          <>
-            <SearchBar />
-          </>
-        )}
+        <SearchBar />
         <ul className="flex space-x-7">
-          <li className="text-gray-200">
-            <div className="border border-gray-400 rounded-md pl-2 pr-2">
-              <button onClick={() => setShowSearch(!showSearch)}>Trade</button>
-            </div>
-          </li>
           <li className="text-gray-200">
             <Link to="/research">Research</Link>
           </li>
